c4bbc5fae040df636e41f97e94c0026624f1f92a
Sayeem25 <assayeem@gmail.com>
2021-04-21 14:16:37 +0600

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from .models import Profile
+# Create your views here.
+def homepage(request):
+
+    profile = Profile.objects.all()
+
+    context = {
+        'profile': profile
+    }
+
+    return render(request, 'index.html', context)
